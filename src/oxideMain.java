db103cbf2e528e8ac7e2de97701fcbd8597dd300
Caleb Morse <caleb.gemorse@gmail.com>
2023-04-18 13:31:26 +0200

    Refactoring the ValueInstance method with an actual node and linked list implementation. Starting with just the default method implementation stubs required from implementing the Node interface.
    Signed-off-by: Caleb Morse - Home <caleb.gemorse@gamil.com>

--- src/oxideMain.java
+++ src/oxideMain.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.Map;
 
 public class oxideMain {
     public static void main(String[] args) {
@@ -8,9 +9,9 @@ public class oxideMain {
         //which then handles the rest of the program
         //oxideMain also handles settings, configurations, presets, and preferences
         boolean isRunning = true;
-//        Parser parser = new Parser("common/buildings/00_buildings.txt");
-//        Map<String, ValueInstance> map = parser.getParsedData();
-//        System.out.println(map);
+        Parser parser = new Parser("common/buildings/00_buildings.txt");
+        Map<String, ValueInstance> map = parser.getParsedData();
+        System.out.println(map);
         String[] options = {"New", "Existing"};
         int newOrExisting = JOptionPane.showOptionDialog(
                 null,
